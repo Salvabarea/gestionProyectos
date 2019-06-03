4e8951a57a2696fd8915678e3b5fd61e15750331
Salva Barea <36158478+Salvabarea@users.noreply.github.com>
2019-06-03 21:52:28 +0200

    Cambido log, añadido el now.
    Revisar subida.php.

--- subida.php
+++ subida.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include('_con.php');
+$nombre = $_SESSION['nombre'];
 
 	$nombre = $_POST['nombreArchivo'];
 	$extension = $_FILES['archivo']['type'];
@@ -10,6 +11,7 @@ include('_con.php');
 	move_uploaded_file($_FILES['archivo']['tmp_name'], "archivos/$nombre.pdf");
 
 	mysqli_query ($conexion, "INSERT INTO proyecto (Nombre, URL, Descripcion) VALUES ('$nombre','archivos/$nombre.pdf','$descripcion')");
-	
+	mysqli_query ($conexion, "INSERT INTO log (Accion, Usuario, Fecha) VALUES ('Subida','$nombre', NOW())");
+
 header("Location: subirProyecto.php");
 ?>
